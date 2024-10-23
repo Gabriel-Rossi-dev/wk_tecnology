5c5410573f7894fcade4c3f964e7c26511c059f1
Gabriel-Rossi-dev <costagabrielrd@gmail.com>
2024-10-23 18:27:46 -0300

    src

---
+++ src/View/CadPedidoVenda.View.Principal.dfm
@@ -0,0 +1,14 @@
+object WK_TECHNOLOGY: TWK_TECHNOLOGY
+  Left = 0
+  Top = 0
+  Caption = 'WK_TECHNOLOGY'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+end
---
+++ src/View/CadPedidoVenda.View.Principal.pas
@@ -0,0 +1,24 @@
+unit CadPedidoVenda.View.Principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TWK_TECHNOLOGY = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  WK_TECHNOLOGY: TWK_TECHNOLOGY;
+
+implementation
+
+{$R *.dfm}
+
+end.
